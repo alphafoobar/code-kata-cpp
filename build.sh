c64da6bfda6545fcdffe97c3f789fbc5f5eaace1
James Little <alphafoobar@users.noreply.github.com>
2019-04-11 22:37:17 +1200

    Create CPP build box with Docker

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# From https://github.com/codecov/example-cpp11-cmake
+
+set -euo pipefail
+
+mkdir -p build && cd build
+
+# Configure
+cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
+# Build (for Make on Unix equivalent to `make -j $(nproc)`)
+cmake --build . --config Debug -- -j $(nproc)
+# Test
+ctest -j $(nproc) --output-on-failure
