cfe580d7af9d86c42ec5c36eae6441f8483045d4
James Little <alphafoobar@users.noreply.github.com>
2019-05-05 22:40:30 +1200

    Simplify point logic and delegate remaining == 1 to recursion

--- src/kata02/binary_search.cpp
+++ src/kata02/binary_search.cpp
@@ -1,30 +1,20 @@
 #include "binary_search.h"
 
-int calculateMid(int offset, int length, int remaining) {
-    int mid = remaining / 2;
-    return static_cast<int>(fminl(length - 1 - offset, mid));
-}
-
 int chop(int key, int array[], int offset, int length) {
     const int remaining = length - offset;
 
     if (remaining == 0) {
         return -1;
     }
-    if (remaining == 1) {
-        return array[offset] == key ? offset : -1;
-    }
 
-    int midPoint = calculateMid(offset, length, remaining);
-
-    int point = offset + midPoint;
+    int point = offset + remaining / 2;
     if (array[point] == key) {
         return point;
     }
     if (array[point] > key) {
         return chop(key, array, offset, point);
     }
-    return chop(key, array, point, length);
+    return chop(key, array, point + 1, length);
 }
 
 int chop(int key, int array[], int length) {
